c5f8b3065c5738d45ad887ef4793e5e8d351fa41
Antonio Supan <antonio.supan@fina.hr>
2022-05-12 15:38:24 +0200

    styles for homepage

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./navbar.scss";
+import { BsSearch } from "react-icons/bs";
+import {
+  MdOutlineLanguage,
+  MdOutlineDarkMode,
+  MdOutlineNotificationsNone,
+  MdChatBubbleOutline,
+  MdList,
+  MdFullscreenExit,
+} from "react-icons/md";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <BsSearch />
+        </div>
+        <div className="items">
+          <div className="item">
+            <MdOutlineLanguage className="icon" />
+            English
+          </div>
+          <div className="item">
+            <MdOutlineDarkMode className="icon" />
+          </div>
+          <div className="item">
+            <MdOutlineNotificationsNone className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <MdChatBubbleOutline className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <MdList className="icon" />
+          </div>
+          <div className="item">
+            <MdFullscreenExit className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/11293719/pexels-photo-11293719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt="avatar"
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,79 @@
 import React from "react";
 import "./sidebar.scss";
-
+import { MdDashboard, MdProductionQuantityLimits } from "react-icons/md";
+import { AiOutlineUser, AiOutlineBell } from "react-icons/ai";
+import { GiPapers } from "react-icons/gi";
+import { ImStatsDots, ImProfile } from "react-icons/im";
+import { GrSystem, GrNotes } from "react-icons/gr";
+import { FiSettings } from "react-icons/fi";
+import { CgLogOut } from "react-icons/cg";
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">supanadmin</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <MdDashboard className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <AiOutlineUser className="icon" />
+            <span>Users</span>
+          </li>
+          <li>
+            <MdProductionQuantityLimits className="icon" />
+            <span>Products</span>
+          </li>
+          <li>
+            <GiPapers className="icon" />
+            <span>Orders</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <ImStatsDots className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <AiOutlineBell className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <GrSystem className="icon" />
+            <span>System health</span>
+          </li>
+          <li>
+            <GrNotes className="icon" />
+            <span>Logs</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <FiSettings className="icon" />
+            <span>Settings</span>
+          </li>
+          <li>
+            <ImProfile className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <CgLogOut className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
